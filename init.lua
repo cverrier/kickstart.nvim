2882585f537016d71dc6d8016e13d36500be9fde
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2024-08-31 11:54:15 +0800

    add venv-selector.nvim
    Ref: https://github.com/linux-cultist/venv-selector.nvim/tree/regexp
    
    (they recommend to use the regexp branch for new users)

--- init.lua
+++ init.lua
@@ -423,6 +423,24 @@ require('lazy').setup({
     },
   },
 
+  {
+    'linux-cultist/venv-selector.nvim',
+    dependencies = {
+      'neovim/nvim-lspconfig',
+      'mfussenegger/nvim-dap',
+      'mfussenegger/nvim-dap-python', --optional
+      { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+    },
+    lazy = false,
+    branch = 'regexp', -- This is the regexp branch, use this for the new version
+    config = function()
+      require('venv-selector').setup()
+    end,
+    keys = {
+      { ',v', '<cmd>VenvSelect<cr>' },
+    },
+  },
+
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
   --    require('gitsigns').setup({ ... })
