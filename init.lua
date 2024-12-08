8ad5fcd6f2449e4079dcf07059f09309c12b3746
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2024-12-08 18:05:15 +0800

    add clang-format formatter

--- init.lua
+++ init.lua
@@ -742,6 +742,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
