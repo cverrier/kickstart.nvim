c09afb71a1c350a97bd3efa8dfa80907e7a9e994
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2024-12-09 06:20:32 +0800

    add Bazel linter

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,8 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        bazel = { 'buildifier' },
+        bzl = { 'buildifier' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
