409ec8e62cbcd9207e8ea4d6989ddfc7f1c75d58
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2024-12-14 21:40:20 +0800

    make buildifier work correctly for formatting

--- init.lua
+++ init.lua
@@ -697,6 +697,7 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
         'yamllint', -- Used to lint YAML code
         'prettier', -- Used to format YAML code
+        'buildifier', -- Used to lint and format Bazel (Starlark) code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -751,6 +752,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         c = { 'clang-format' },
         yaml = { 'prettier' },
+        bzl = { 'buildifier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
