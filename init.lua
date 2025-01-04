f87589d3add1bb9cc22558ebcfca2100156c9555
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-01-04 15:23:09 +0800

    add vimtex plugin and update

--- init.lua
+++ init.lua
@@ -296,6 +296,15 @@ require('lazy').setup({
       },
     },
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
 
   {
     'nvim-tree/nvim-tree.lua',
