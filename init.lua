6b00100eb33b2f2f472573fbe68df3f0b1d896fa
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-07-02 17:43:07 +0800

    update

--- init.lua
+++ init.lua
@@ -679,13 +679,13 @@ require('lazy').setup({
       local servers = {
         clangd = {
           cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--header-insertion=iwyu",
-            "--completion-style=detailed",
-            "--function-arg-placeholders",
-            "--fallback-style=llvm",
+            'clangd',
+            '--background-index',
+            '--clang-tidy',
+            '--header-insertion=iwyu',
+            '--completion-style=detailed',
+            '--function-arg-placeholders',
+            '--fallback-style=llvm',
           },
           init_options = {
             usePlaceholders = true,
