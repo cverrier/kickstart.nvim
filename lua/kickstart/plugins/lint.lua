efcd7e4d46de4e68d29f92e5867256205b974e5c
Clement Verrier <17490658+cverrier@users.noreply.github.com>
2025-10-03 10:12:28 +0200

    add ruff linter

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -9,6 +9,7 @@ return {
         markdown = { 'markdownlint' },
         c = { 'clangtidy' },
         cpp = { 'clangtidy' },
+        python = { 'ruff' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
